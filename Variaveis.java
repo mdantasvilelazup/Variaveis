e94dadd4cced3a881f6a3e032172efbdd1f33538
Matheus Dantas <matheus.vilela@zup.com.br>
2024-12-02 15:25:41 -0300

    atv variaveis

---
+++ Variaveis.java
@@ -0,0 +1,11 @@
+public class Variaveis {
+    public static void main(String args[]) {
+        int age = 21;
+        String name = "dantas";
+        float height = 1.8f;
+        boolean isSigned = true;
+
+        System.out.println("Nome: " + name + ", Idade: " + age + ", Altura: " + height + ", Matrícula: " + isSigned);
+
+    }
+}
